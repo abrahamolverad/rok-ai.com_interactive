ad2d454bc5a8f983d32642a549a97387f2eae5c6
abrahamolverad <abrahamolvera_a@hotmail.com>
2025-05-14 15:33:01 +0400

    Add cache-busting headers to prevent browser caching

--- server.js
+++ server.js
@@ -4,8 +4,28 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware to add cache-busting headers
+app.use((req, res, next) => {
+  // Prevent caching
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  res.setHeader('Surrogate-Control', 'no-store');
+  next();
+});
+
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+  // Also set cache control headers for static files
+  etag: false,
+  lastModified: false,
+  setHeaders: (res) => {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    res.setHeader('Surrogate-Control', 'no-store');
+  }
+}));
 
 // All routes go to index.html
 app.get('*', (req, res) => {
