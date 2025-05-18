16c534a5070ba88bb4808b2b82a43cf14e0b6146
abrahamolverad <abrahamolvera_a@hotmail.com>
2025-05-18 10:35:38 +0400

    fix(auth): add NextAuth API endpoint as dotdotdotnextauth.ts

---
+++ pages/api/auth/dotdotdotnextauth.ts
@@ -0,0 +1,23 @@
+﻿import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      authorize: async (credentials) => {
+        // Replace with your logic
+        if(credentials?.username === 'demo' && credentials?.password === 'demo') {
+          return { id: '1', name: 'Demo User', email: 'demo@example.com' };
+        }
+        return null;
+      }
+    })
+  ],
+  session: { strategy: 'jwt' },
+  secret: process.env.NEXTAUTH_SECRET || 'super-secret'
+});
